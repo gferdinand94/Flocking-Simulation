08e22aed3e77f1a030eb17ec0b8d5d4d727ff4ce
gferdinand94 <98139166+gferdinand94@users.noreply.github.com>
2024-10-03 10:38:22 -0500

    second commit

--- obj/Debug/net6.0/FlockingSimulation.AssemblyInfo.cs
+++ obj/Debug/net6.0/FlockingSimulation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlockingSimulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f379e8416d70600731d89437ad9144a86e0b929")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlockingSimulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlockingSimulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
